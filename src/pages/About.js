95c9a038e158b7ff070f37b7f6daa4df247737b0
mhhabib <mhrahman7096@gmail.com>
2024-01-31 22:41:40 +0600

    added new ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
-import { MenuItem, Menu } from 'semantic-ui-react'
 
 const About=()=>{
-    const [menuItem, setMenuItem] = useState({activeItem: 'home' })
-    const handleItemClick=(e, {name})=>{
-        e.preventDefault();
-        console.log("Name: ", name)
-        setMenuItem({activeItem: name})
-    }
-    return <div className="about-me">
-        <Menu inverted widths={3} activeItem={menuItem.activeItem}>
-            <MenuItem
-                name='home'
-                active={menuItem.activeItem === 'home'}
-                onClick={handleItemClick}
-            />
-            <MenuItem
-                name='messages'
-                active={menuItem.activeItem === 'messages'}
-                onClick={handleItemClick}
-            />
-            <MenuItem
-                name='friends'
-                active={menuItem.activeItem === 'friends'}
-                onClick={handleItemClick}
-                to="/"
-            />
-        </Menu>
-    </div>
+   
+    return <>
+        <div className="bg-[#232425] w-full h-screen text-[#a9a9b3]">
+            <p className="mb-8 py-8 text-lg text-center font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                #<span className="text-[#e06c75] font-bold">Home </span> 
+                #<span className="text-[#1AACAC]">Experience </span> 
+                #<span className="text-[#e4bb68]">Eduction </span>
+                #<span className="text-[#789461]">Social media </span>
+                #<span className="text-white">CV</span>
+            </p>
+        </div>
+    </>
 }
 export default About;
--- src/pages/NotFound404.js
+++ src/pages/NotFound404.js
@@ -2,13 +2,16 @@ import React, { Fragment } from "react";
 
 const NotFound404=()=>{
     return <Fragment>
-        <div className="main-body">
-            <div className="notfound">
-                <h1>4<span style={{color: '#F28585'}}>0</span>4</h1>
-                <p>Sorry, we can't find this page. You'll find lots to explore on the home page.</p>
-                <p ><a href="/" style={{color: '#088395', fontSize: '18px'}}>&#8592; Back to Home</a></p>
-            </div>
-        </div>
+        <div className="h-screen flex flex-col h-screen items-center justify-center px-4 py-16 sm:px-6 lg:px-8 bg-[#232425] w-full h-screen text-[#a9a9b3]">
+			<div className=" mx-auto max-w-lg">
+				<h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 text-center">
+                    4<span className="text-red-600">0</span>4
+                </h1>
+                <p className="mb-4 text-3xl tracking-tight font-bold text-[#a9a9b3] md:text-4xl dark:text-white text-center">Something's missing.</p>
+                <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400 text-center">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                <a href="/" className="block mx-auto mt-4 max-w-md rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white text-center">Back to Homepage</a>
+			</div>
+		</div>
     </Fragment>
 }
 export default NotFound404;
