6df817802d6220ca5be971f743ad34916d9003ff
mhhabib <mhrahman7096@gmail.com>
2024-02-01 11:16:09 +0600

    added next page link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -74,7 +74,7 @@ const Home=()=>{
                         <div className="mx-auto max-w-screen-xl text-center">
                         <span className="hidden md:inline">Page size - 99kb</span>
                         <span className="md:hidden">
-                        <a href="#" className="inline-flex mb-4 px-3 text-sm font-normal leading-none text-blue-700 selection:bg-gray-600 selection:text-pink-400">Next
+                        <a href="/me" className="inline-flex mb-4 px-3 text-sm font-normal leading-none text-blue-700 selection:bg-gray-600 selection:text-pink-400">Next
                             <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                             </svg>
