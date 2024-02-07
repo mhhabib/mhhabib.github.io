960540dd11c739e90889208ca71e05b7d06c6cb2
mhhabib <mhrahman7096@gmail.com>
2024-02-07 23:35:55 +0600

    fix bug

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,6 @@ const About=()=>{
     return <>
         <div className="bg-[#232425] w-full min-h-screen text-[#a9a9b3]">
             <p className="mb-8 pt-16  text-lg text-center font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-                #<span className="text-[#e06c75]"><Link to="/">Home</Link> </span> 
                 #<span className="text-[#e06c75]"><Link to="experience">Experience</Link> </span> 
                 #<span className="text-[#1AACAC]"><Link to="education">Education</Link> </span> 
                 #<span className="text-[#e4bb68]"><Link to="projects">Projects</Link> </span>
