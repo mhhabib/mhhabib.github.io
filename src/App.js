3c19841cc7c6dd117ff162408b798b7ebe95044a
mhhabib <mhrahman7096@gmail.com>
2024-02-07 12:09:08 +0600

    Added eduction and experience section

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound404 from './pages/NotFound404';
+import Experience from './pages/Experience';
+import Education from './pages/Education';
 
 function App() {
   return (
     <Fragment>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/me" element={<About />} />
+        <Route path="/me" element={<About />} >
+          <Route index element={<Experience />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="education" element={<Education />} />
+        </Route>
         <Route path="*" element={<NotFound404/>}/>
       </Routes>
     </Fragment>
---
+++ src/pages/Education.js
@@ -0,0 +1,56 @@
+
+const Education = ()=>{
+    return <>
+        <div className="flex flex-col items-center justify-center p-8">
+            <ol className="relative border-s border-gray-200 dark:border-gray-700">                  
+                <li className="mb-10 ms-6">            
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                    </span>
+                    
+                    <h3 className="flex items-center px-3 text-xl font-semibold text-[#04AA6D] selection:bg-gray-600">BSc in Engg in Computer Science & Engineering <span className="px-3 text-sm font-normal leading-none text-gray-400">January 2015 - June 2019</span></h3>
+                    <a href="https://just.edu.bd/" target="_blank" rel="noreferrer" className="inline-flex mb-4 px-3 text-sm font-normal leading-none text-[#ff9999] selection:bg-gray-600 selection:text-pink-400">Jashore University of Science and Technology, Jashore, Bangladesh
+                        <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                    
+                    <ul className="max-w-screen-md mb-4 px-3 text-base font-normal text-[#B4B4B8] space-y-1 text-gray-500 list-disc list-inside selection:bg-gray-600 selection:text-gray-100">
+                        <li>
+                                Get started with a collection of list components built with Tailwind CSS for ordered and unordered lists with bullets, numbers, or icons and other styles and layouts to show a list of items inside an article or throughout your web page.
+                        </li>
+                        <li>
+                                Get started with a collection of list components built with Tailwind CSS for ordered and unordered lists with bullets, numbers, or icons and other styles and layouts to show a list of items inside an article or throughout your web page.
+                        </li>
+                    </ul>
+                </li>
+                <li className="mb-10 ms-6">
+                    <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                        <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                        </svg>
+                    </span>
+                    
+                    <h3 className="flex items-center px-3 text-xl font-semibold text-[#04AA6D] selection:bg-gray-600">Higher Secondary Certificate <span className="px-3 text-sm font-normal leading-none text-gray-400">June 2011 - April 2013</span></h3>
+                    <a href="https://kushtiagovcollege.edu.bd/" target="_blank" rel="noreferrer" className="inline-flex mb-4 px-3 text-sm font-normal leading-none text-[#ff9999] selection:bg-gray-600 selection:text-pink-400">Kushtia Government College, Kushtia, Bangladesh
+                        <svg className="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                    
+                    <ul className="max-w-screen-md mb-4 px-3 text-base font-normal text-[#BFCFE7] space-y-1 text-gray-500 list-disc list-inside selection:bg-gray-600 selection:text-gray-100">
+                        <li>
+                                Get started with a collection of list components built with Tailwind CSS for ordered and unordered lists with bullets, numbers, or icons and other styles and layouts to show a list of items inside an article or throughout your web page.
+                        </li>
+                        <li>
+                                Get started with a collection of list components built with Tailwind CSS for ordered and unordered lists with bullets, numbers, or icons and other styles and layouts to show a list of items inside an article or throughout your web page.
+                        </li>
+                    </ul>
+                </li>
+            </ol>
+        </div> 
+    </>
+}
+export default Education
