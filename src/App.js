87ef4b5c81b7f30e8d36734ce2a018909b2676ce
mhhabib <mhrahman7096@gmail.com>
2024-02-08 20:12:49 +0600

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
@@ -7,7 +7,7 @@ import NotFound404 from './pages/NotFound404';
 import Experience from './pages/Experience';
 import Education from './pages/Education';
 import Resume from './pages/Resume';
-import GithubRepository from './pages/GithubRepos';
+const GithubRepository = React.lazy(()=>import("./pages/GithubRepos"))
 
 function App() {
   return (
@@ -18,7 +18,11 @@ function App() {
           <Route index element={<Experience />} />
           <Route path="experience" element={<Experience />} />
           <Route path="education" element={<Education />} />
-          <Route path="projects" element={<GithubRepository />} />
+          <Route path="projects" element={
+            <React.Suspense fallback={<></>}>
+              <GithubRepository/>
+            </React.Suspense>
+          } />
           <Route path="cv" element={<Resume />} />
         </Route>
         <Route path="*" element={<NotFound404/>}/>
