b708735ff1d3dde96385760610c2db0dabdb9354
mhhabib <mhrahman7096@gmail.com>
2024-02-11 16:55:17 +0600

    Added project loading spinner

--- src/pages/GithubRepos.js
+++ src/pages/GithubRepos.js
@@ -2,12 +2,15 @@ import React, {useEffect, useState} from "react";
 import Repository from "./Repository";
 const GithubRepository=()=>{
     const [repositories, setRepositories]=useState([])
+    const [isLoading, setIsLoading]=useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         fetch("https://api.github.com/users/mhhabib/repos?page=1&per_page=1000")
           .then(res => res.json())
           .then(data => {
             setRepositories(data.filter(repo => repo.stargazers_count > 0 || repo.forks_count > 0))
+            setIsLoading(false)
           })
     },[]);
 
@@ -16,7 +19,16 @@ const GithubRepository=()=>{
       }
     );
     repositories.reverse();
-
+    
+    if(isLoading){
+        return <div className="flex items-center justify-center h-screen">
+                <div className="relative">
+                    <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+                    <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-green-500 animate-spin">
+                    </div>
+                </div>
+            </div>
+    }
     return <>
         <div className="flex flex-col items-center justify-center p-8">
             <div className="w-screen md:max-w-screen-lg mb-4 px-3 grid grid-cols-2 md:grid-cols-3 gap-4">
