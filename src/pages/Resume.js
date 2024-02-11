373a076182b9f13bcae7b8d6b4acc1c967363130
mhhabib <mhrahman7096@gmail.com>
2024-02-11 14:55:20 +0600

    Open pdf viewer for all devices

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -4,7 +4,6 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import latestResume from "../files/CV_Md_Habibur_Rahman.pdf"
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import {detectDeviceAndOs} from "../Utils/Utils"
 
 const Resume=()=>{
     const defaultLayoutPluginInstance = defaultLayoutPlugin({
@@ -32,18 +31,14 @@ const Resume=()=>{
             </Toolbar>
         )
     });
-    if (detectDeviceAndOs().isMobile) {
-        return <div className="text-center mt-8">CV viewer is available only on PC devices.</div>;
-    }
-    
     return <>
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-            <div className="flex flex-col items-center justify-center p-8 text-center">
-                <div
+            <div className="flex flex-col md:items-center md:justify-center p-8 text-center">
+                <div className='w-full md:w-[60%]'
                     style={{
                         border: '1px solid rgba(0, 0, 0, 0.3)',
                         height: '750px',
-                        width: '60%',
+
                     }}>   
                          <Viewer fileUrl={latestResume}  plugins={[defaultLayoutPluginInstance]} />
                 </div>
